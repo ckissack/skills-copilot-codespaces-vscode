ca81e44a195169c855d3979842c6bc63e80a27c6
ckissack <140193026+ckissack@users.noreply.github.com>
2023-07-21 17:47:20 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: "John Doe",
+        age: 25,
+        skills: ["JavaScript", "CSS", "HTML"]
+    };
+    console.log(member);
+    return member;
+}
